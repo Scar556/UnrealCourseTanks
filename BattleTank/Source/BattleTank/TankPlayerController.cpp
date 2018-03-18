979cc18375e3d86d826b39660f6086a4aade502e
THEBEAST\Will <wjfwilson2010@googlemail.com>
2018-03-18 17:28:35 +0200

    Added framework for an out parameter

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -38,7 +38,18 @@ ATank* ATankPlayerController::GetControlledTank() const
 void ATankPlayerController::AimTowardsCrosshair()
 {
 	if (!GetControlledTank()) { return; }
-	//Get world location if you line trace through crosshair
-	//If it hits the landscape
-		// Tell controlled tank to aim at this point
+
+	FVector HitLocation; // Out Parameter
+	if (GetSightRayHitLocation(HitLocation)) // Has "side-effect", is going to line trace
+	{
+	UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString())
+			// TODO Tell controlled tank to aim at this point
+	}
+}
+
+//Get world location of the line trace through crosshair, true if it hits landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
+{
+	HitLocation = FVector(1.0f);
+	return true;
 }
