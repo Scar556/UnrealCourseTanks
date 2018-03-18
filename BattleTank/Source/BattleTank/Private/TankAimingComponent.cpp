99baccab248150afd2ed9a32fd97a7470a265207
THEBEAST\Will <wjfwilson2010@googlemail.com>
2018-03-18 21:59:09 +0200

    Fixed previous issue with aiming logging. Also added another component that affects a Static Mesh

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -14,6 +14,13 @@ UTankAimingComponent::UTankAimingComponent()
 }
 
 
+
+
+void UTankAimingComponent::SetBarrelReference(UStaticMeshComponent * BarrelToSet)
+{
+	Barrel = BarrelToSet;
+}
+
 // Called when the game starts
 void UTankAimingComponent::BeginPlay()
 {
@@ -35,7 +42,8 @@ void UTankAimingComponent::TickComponent(float DeltaTime, ELevelTick TickType, F
 void UTankAimingComponent::AimAt(FVector HitLocation)
 {
 
-		auto OurTankName = GetName();
-		UE_LOG(LogTemp, Warning, TEXT("%s is aiming at %s"), *OurTankName, *HitLocation.ToString())
+		auto OurTankName = GetOwner()->GetName();
+		auto BarrelLocation = Barrel->GetComponentLocation().ToString();
+		UE_LOG(LogTemp, Warning, TEXT("%s is aiming at %s from %s"), *OurTankName, *HitLocation.ToString(), *BarrelLocation)
 
 }
