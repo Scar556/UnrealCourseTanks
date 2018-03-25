d2d6e8947558ca6114a7dce7c152a680b92377db
THEBEAST\Will <wjfwilson2010@googlemail.com>
2018-03-25 17:50:43 +0300

    Added joystick movement and trigger firing method

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,12 +1,24 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankMovementComponent.h"
+#include "TankTracks.h"
 
+void UTankMovementComponent::Initialise(UTankTracks* LeftTrackToSet, UTankTracks* RightTrackToSet)
+{
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-	// auto Name = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("Intend Move Forward throw: %f"), Throw)
-	
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
 }
 
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
+}
